0b05b7773abb91e5414af69dde6680b80a623ec5
Kernael92 <kernaelapuko5@gmail.com>
2018-09-23 22:07:34 +0300

    add the corrections made to the html and script file

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
-var question1Answers = prompt("Inside which HTML element do we put the JavaScript?"); //I'm going to type JavaScript, script, js //
-question1Answers = ("A) JavaScript", "B)script", "C)js");
-var question2Aanswers = prompt("How do you declare a JavaScript variable?");  //I'm going to type V carName;, variable carName;, var carName; //
+var question1Answers = prompt("Inside which HTML element do we put the JavaScript?");
+question1Answers = ("A) JavaScript", "B) script", "C) js");
+var question2Aanswers = prompt("How do you declare a JavaScript variable carName;?");
 question2Aanswers = ("A) v carName;", "B) variable carName;", "C) var carName;");
 var question3Answers = promp("Which event occurs when the user clicks on an HTML element");
 question3Answers  = alert(" A) onmouseover", "B) onclick", "C) onchange");
@@ -24,35 +24,35 @@ question5Answers = alert("A) msg(Hello World)", "B) alertBox(Hello World)", "C) 
 $(document).ready(function() {
    $(form#quiz).submit(function(event){
       event.preventDefault();
-      var answer1 = $("input:radio[name=question-1-answers]:checked").val();
+      var answer1 = $("input:radio[name=question-1-answers]").val();
       $("#output").text(result);
    }):
 }):
 $(document).ready(function() {
    $(form#quiz).submit(function(event){
       event.preventDefault();
-      var answer2 = $("input:radio[name=question-2-answers]:checked").val();
+      var answer2 = $("input:radio[name=question-2-answers]:").val();
       $("#output").text(result);
    }):
 }):
 $(document).ready(function() {
    $(form#quiz).submit(function(event){
       event.preventDefault();
-      var answer3 = $("input:radio[name=question-3-answers]:checked").val();
+      var answer3 = $("input:radio[name=question-3-answers]:").val();
       $("#output").text(result);
    }):
 }):
 $(document).ready(function() {
    $(form#quiz).submit(function(event){
       event.preventDefault();
-      var answer4 = $("input:radio[name=question-4-answers]:checked").val();
+      var answer4 = $("input:radio[name=question-4-answers]:").val();
       $("#output").text(result);
    }):
 }):
 $(document).ready(function() {
    $(form#quiz).submit(function(event){
       event.preventDefault();
-      var answer5 = $("input:radio[name=question-5-answers]:checked").val();
+      var answer5 = $("input:radio[name=question-5-answers]:").val();
       $("#output").text(result);
    }):
 }):
