869c93184d860c3b47e73001d046c8629ab096d5
Kernael92 <kernaelapuko5@gmail.com>
2018-09-23 18:28:14 +0300

    add the business logic and user interface to the script file

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,53 @@
+var question1Answers = ("answer1");
+answer1 = alert("A","B","C");;
+var question2Aanswers = ("answer2");
+answer2 = ("A","B","C");
+var question3Answers = ("answer3");
+answer1 = alert("A","B","C");
+var question4Answers = ("answer4");
+answer4 = alert("A","B","C");
+var question5Answers = ("answer5");
+answer5 = alert("A","B","C");
 
+
+
+
+
+
+
+
+
+
+
+
+
+$(document).ready(function() {
+   $(form#quiz).submit(function(event){
+      event.preventDefault();
+      var answer1 = $("input:radio[name=question-1-answers]:checked").val();
+   }):
+}):
+$(document).ready(function() {
+   $(form#quiz).submit(function(event){
+      event.preventDefault();
+      var answer2 = $("input:radio[name=question-2-answers]:checked").val();
+   }):
+}):
+$(document).ready(function() {
+   $(form#quiz).submit(function(event){
+      event.preventDefault();
+      var answer3 = $("input:radio[name=question-3-answers]:checked").val();
+   }):
+}):
+$(document).ready(function() {
+   $(form#quiz).submit(function(event){
+      event.preventDefault();
+      var answer4 = $("input:radio[name=question-4-answers]:checked").val();
+   }):
+}):
+$(document).ready(function() {
+   $(form#quiz).submit(function(event){
+      event.preventDefault();
+      var answer5 = $("input:radio[name=question-5-answers]:checked").val();
+   }):
+}):
