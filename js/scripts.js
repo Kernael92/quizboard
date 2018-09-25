48fd8dd755c01ab71fef37b53bda2f179b507d83
Kernael92 <kernaelapuko5@gmail.com>
2018-09-25 12:48:04 +0300

    change the submit buttons in q1-q4 to btn1-btn4 in html file and use the methods show() and hide() to show the questions and hide the buttons

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,8 @@ var scrore = 0; //set score to 0
 var total = 5; //Total number of questions
 var point = 1; //points per correct answer
 var highest = total * point;
-
+var n = total;
+var score = highest;
 function init () {
    // set correct answers
    sessionStorage.setItem("A1","B");
@@ -15,54 +16,90 @@ function init () {
 }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 $(document).ready(function() {
-   //Hide all questions
-   $('.questionForm').hide();
    //show first question
    $("#q1").show();
-   $("q1 #submit").click(function() {
-      $("questionForm").hide();
-      $('#q2').fadeIn(200);
-      return false;
-   });
-   $("q2 #submit").click(function() {
-      $("questionForm").hide();
-      $('#q3').fadeIn(200);
-      return false;
-   });
-   $("q3 #submit").click(function() {
-      $("questionForm").hide();
-      $('#q4').fadeIn(200);
-      return false;
-   });
-   $("q4 #submit").click(function() {
-      $("questionForm").hide();
-      $('#q5').fadeIn(200);
-      return false;
-   });
-   $("q5 #submit").click(function() {
-      $("questionForm").hide();
-      $('#results').fadeIn(200);
-      return false;
+
+   $('#q2').hide();
+
+   $('#q3').hide();
+
+   $('#q4').hide();
+
+   $('#q5').hide();
+
+   $('#btn1').click(function(){
+      $('#q2').show();
+      $('#btn1').hide();
    });
 
+   $('#btn2').click(function(){
+      $('#q3').show();
+      $('#btn2').hide();
+   });
+   $('#btn3').click(function(){
+      $('#q4').show();
+      $('#btn3').hide();
+   });
+   $('#btn4').click(function(){
+      $('#q5').show();
+      $('#btn4').hide();
+   });
+
+   $("form.questionForm").submit(function(event){
+      event.preventDefault();
+
+
+   $(".questionFor#submit").click(function() {
+
+      //get data attribute
+      current = $(this).parents("form:first").data("question");
+      next = $(this).parents("form:first").data("question")+1;
+      //Hide all questions
+      // $('.questionForm').hide();
+      //show next question
+      $("#q" +next+"").fadeIn(200);
+      process("+current+");
+      return false;
+
+   });
+   });
 });
+
+
+
+
+
+
+
+
+//process the answers
+function process(n){
+   var submitted = $("input[name=q'+n+']:checked").val();
+   if(submitted = sessionStorage.getItem("A"+n+'')){
+      score + score + point
+   }
+
+
+
+   }
+
+if (n == total){
+   $("#results").html("<h2>Your final score is:"+score+ "out of"  + "highest"+"</h2><a href=\"index.html\">Take Quiz");
+}
+if (score == highest){
+   $("#results").append("<p>You are a JavaScript guru!</p>");
+} else if (score == highest - point || score == highest - point - point){
+   $("#results").append("<p>Good job</p>");
+} else{
+
+   alert ("false");
+}
+
+
+
+
+
+
 //Add event listener
 window.addEventListener("load", init, false);
