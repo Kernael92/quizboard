350523fa35e6a123230e0cebcf3fb6f2f802b435
Kernael92 <kernaelapuko5@gmail.com>
2018-09-25 05:01:43 +0300

    use jQuery to hide thequestions apart from q1 which when answred and the submit button clicked takes us to q2 and so on..

---
+++ js/scripts.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+   //Hide all questions
+   $('.questionForm').hide();
+   //show first question
+   $("#q1").show();
+   $("q1 #submit").click(function() {
+      $("questionForm").hide();
+      $('#q2').fadeIn(200);
+      return false;
+   });
+   $("q2 #submit").click(function() {
+      $("questionForm").hide();
+      $('#q3').fadeIn(200);
+      return false;
+   });
+   $("q3 #submit").click(function() {
+      $("questionForm").hide();
+      $('#q4').fadeIn(200);
+      return false;
+   });
+   $("q4 #submit").click(function() {
+      $("questionForm").hide();
+      $('#q5').fadeIn(200);
+      return false;
+   });
+   $("q5 #submit").click(function() {
+      $("questionForm").hide();
+      $('#results').fadeIn(200); 
+      return false;
+   });
+
+});
